d78f2c33a63b7e840ac8fb1c4e177b94563e200f
voltan <djvoltan@gmail.com>
2018-03-05 11:41:54 +0330

    Add paginator

--- src/Controller/Admin/DictionaryController.php
+++ src/Controller/Admin/DictionaryController.php
@@ -13,10 +13,10 @@
 
 namespace Module\Search\Controller\Admin;
 
-use Module\Guide\Form\RegenerateImageForm;
-use Module\Guide\Form\SitemapForm;
 use Pi;
 use Pi\Mvc\Controller\ActionController;
+use Pi\Paginator\Paginator;
+use Zend\Db\Sql\Predicate\Expression;
 
 class DictionaryController extends ActionController
 {
@@ -24,22 +24,51 @@ class DictionaryController extends ActionController
     {
         // Get info from url
         $module = $this->params('module');
+        $page   = $this->params('page', 1);
+
         // Get config
         $config = Pi::service('registry')->config->read($module);
+
         // Set list
         $list = [];
         if ($config['save_log']) {
+
+            // Set info
+            $limit  = 100;
+            $offset = (int)($page - 1) * $limit;
             $order  = ['weight DESC', 'id DESC'];
-            $select = $this->getModel('dictionary')->select()->order($order)->limit(100);
+            $select = $this->getModel('dictionary')->select()->order($order)->offset($offset)->limit($limit);
             $rowset = $this->getModel('dictionary')->selectWith($select);
+
             // Make list
             foreach ($rowset as $row) {
                 $list[$row->id] = $row->toArray();
             }
+
+            // Set count
+            $columns = ['count' => new Expression('count(*)')];
+            $select  = $this->getModel('dictionary')->select()->columns($columns);
+            $count   = $this->getModel('dictionary')->selectWith($select)->current()->count;
+
+            // Set paginator
+            $paginator = Paginator::factory(intval($count));
+            $paginator->setItemCountPerPage($limit);
+            $paginator->setCurrentPageNumber($page);
+            $paginator->setUrlOptions([
+                'router' => $this->getEvent()->getRouter(),
+                'route'  => $this->getEvent()->getRouteMatch()->getMatchedRouteName(),
+                'params' => array_filter([
+                    'module'     => $this->getModule(),
+                    'controller' => 'dictionary',
+                    'action'     => 'index',
+                ]),
+            ]);
         }
+
         // Set template
         $this->view()->setTemplate('dictionary-index');
         $this->view()->assign('list', $list);
         $this->view()->assign('config', $config);
+        $this->view()->assign('paginator', $paginator);
     }
 }
