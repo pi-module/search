4a5a368f8b1882fb366018d7b73e3558a93635aa
Hossein Azizabadi Farahani <djvoltan@gmail.com>
2019-11-03 14:42:21 +0330

    Reformat codes

--- sql/mysql.sql
+++ sql/mysql.sql
@@ -8,23 +8,25 @@
 # >>>>
 
 # Search log
-CREATE TABLE `{log}` (
-  `id`   INT(10) UNSIGNED NOT NULL    AUTO_INCREMENT,
-  `term` VARCHAR(255)     NOT NULL    DEFAULT '',
-  `uid`  INT(10) UNSIGNED NOT NULL    DEFAULT '0',
-  `ip`   CHAR(15)         NOT NULL    DEFAULT '',
-  `time` INT(10) UNSIGNED NOT NULL    DEFAULT '0',
-  PRIMARY KEY (`id`),
-  KEY `uid` (`uid`),
-  KEY `term` (`term`)
+CREATE TABLE `{log}`
+(
+    `id`   INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `term` VARCHAR(255)     NOT NULL DEFAULT '',
+    `uid`  INT(10) UNSIGNED NOT NULL DEFAULT '0',
+    `ip`   CHAR(15)         NOT NULL DEFAULT '',
+    `time` INT(10) UNSIGNED NOT NULL DEFAULT '0',
+    PRIMARY KEY (`id`),
+    KEY `uid` (`uid`),
+    KEY `term` (`term`)
 );
 
 # Search auto complete
-CREATE TABLE `{dictionary}` (
-  `id`     INT(10) UNSIGNED NOT NULL    AUTO_INCREMENT,
-  `term`   VARCHAR(255)     NOT NULL    DEFAULT '',
-  `weight` INT(10) UNSIGNED NOT NULL    DEFAULT '0',
-  PRIMARY KEY (`id`),
-  KEY `weight` (`weight`),
-  KEY `term` (`term`)
+CREATE TABLE `{dictionary}`
+(
+    `id`     INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `term`   VARCHAR(255)     NOT NULL DEFAULT '',
+    `weight` INT(10) UNSIGNED NOT NULL DEFAULT '0',
+    PRIMARY KEY (`id`),
+    KEY `weight` (`weight`),
+    KEY `term` (`term`)
 );
